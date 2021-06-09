60009c4f4db8e919d588d2b640348d132adc71b4
tejaswirajput <rajput3045@gmail.com>
2021-06-09 11:20:41 +0530

    first commit
    Signed-off-by: tejaswirajput <rajput3045@gmail.com>

---
+++ ex/Lab1Ex4.java
@@ -0,0 +1,29 @@
+package com.lab.ex;
+
+	import java.util.Scanner;
+
+	public class Lab1Ex4 {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the range of fibonacci :- ");
+			int num = sc.nextInt();
+			sc.close();
+			for (int n = 2; n < num; n++) {
+
+				int flag = 0;
+
+				for (int i = 2; i < n; i++) {
+					if (n % i == 0) {
+
+						flag = 1;
+					}
+				}
+				if (flag == 0) {
+					System.out.println(n);
+				}
+
+			}
+		}
+
+
+}
---
+++ ex/Lab1Ex5.java
@@ -0,0 +1,24 @@
+package com.lab.ex;
+
+	import java.util.Scanner;
+
+	public class Lab1Ex5 {
+		static void calculateSum(int n) {
+			int sum = 0;
+			for (int i = 0; i <= n; i++) {
+				if ((i % 3) == 0 || (i % 5) == 0) {
+					sum = sum + i;
+				}
+			}
+			System.out.println(sum);
+		}
+
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Please Enter an interger - ");
+			int num = sc.nextInt();
+			sc.close();
+			calculateSum(num);
+		}
+
+}
