c41c6279b8c69288626700261fcc2f64c94d80f7
tejaswirajput <rajput3045@gmail.com>
2021-06-09 11:09:07 +0530

    commti
    Signed-off-by: tejaswirajput <rajput3045@gmail.com>

---
+++ ex/Lab11.java
@@ -0,0 +1,27 @@
+package com.lab.ex;
+
+	import java.util.Scanner;
+
+	public class Lab11 {
+		
+		public static void main(String[] args) {
+			
+	        Scanner sc =  new Scanner(System.in);
+			
+			System.out.println("please enter an integer -");
+			int num = sc.nextInt();
+			
+			int rem,sum=0;
+			
+			while(num>0) {
+				rem = num%10;
+				sum = sum+(rem*rem*rem);
+				num = num/10;
+			}
+			System.out.println("Cube is  " + sum);
+			//System.out.println(sum);
+		}
+
+	}
+
+
