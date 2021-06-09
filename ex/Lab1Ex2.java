159cc5a3bed109ca1ed073a13a2501621e4e0d22
tejaswirajput <rajput3045@gmail.com>
2021-06-09 11:14:37 +0530

    Signed-off-by: tejaswirajput <rajput3045@gmail.com>

---
+++ ex/Lab1Ex2.java
@@ -0,0 +1,24 @@
+package com.lab.ex;
+
+import java.util.Scanner;
+public class Lab1Ex2 {
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter the choice red/yellow/green  - ");
+		 String ch = sc.next();
+		 sc.close();
+		 
+		 switch(ch) {
+		 case "red": System.out.println("Light is red you should STOP..!");
+		 			break;
+		 case "green" : System.out.println("You can Go ..");
+		 				break;
+		 case "yellow": System.out.println("Be Ready!");
+		 				break;
+		 default: System.out.println("wrong input");
+		 			
+		 }
+		 
+	}
+
+}
